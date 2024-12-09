9f5b8b58a5fa69782a526050227f6bb0e1f8f6f9
deepikamonga11 <deepeekaa.monga4@gmail.com>
2024-12-09 14:26:30 +0530

    test4

--- modules/storage_account/main.tf
+++ modules/storage_account/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg2" {
 }
 
 resource "azurerm_storage_account" "storage_account" {
-  name                     = "testterraform112233"
+  name                     = "testterraform11222233"
   resource_group_name      = azurerm_resource_group.rg2.name
   location                 = azurerm_resource_group.rg2.location
   account_tier             = "Standard"
