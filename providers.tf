a64b269def7f10ed0732087a0fef11a5dff49e2c
deepikamonga11 <deepeekaa.monga4@gmail.com>
2024-12-09 13:55:03 +0530

    test

--- providers.tf
+++ providers.tf
@@ -13,3 +13,25 @@ provider "azurerm" {
   features {}
   subscription_id = "87948765-5c07-4a92-819f-251defd8eefd"
 }
+data "azurerm_resource_group" "be_resource_group_name" {
+  name = "rsg-shared-001"
+}
+ 
+data "azurerm_storage_account" "be_storage_account" {
+  name                = "myonetfstatesa001"
+  resource_group_name = data.azurerm_resource_group.be_resource_group_name.name
+}
+ 
+data "azurerm_storage_container" "be_storage_container" {
+  name                 = "my-tf-state-blob-001"
+  storage_account_name = data.azurerm_storage_account.be_storage_account.name
+}
+ 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rsg-shared-001"
+    storage_account_name = "myonetfstatesa001"
+    container_name       = "my-tf-state-blob-001"
+    key                  = "terraform.tfstate"
+  }
+}
